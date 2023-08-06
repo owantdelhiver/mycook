c1b943436a8c6af37da14fed35582d9b7fb6bd7a
owen <owenboutet@gmail.com>
2023-08-06 20:08:37 +0200

    ignore .vscode

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping(value = {"/", "/home"})
 	public String index() {
 		return "Salut c'est moi le plus beau!";
 	}
