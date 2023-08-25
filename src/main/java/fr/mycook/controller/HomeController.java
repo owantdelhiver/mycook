09b34e39215893f3aeaf3ce8155a7d462678f7b5
owen <owenboutet@gmail.com>
2023-08-25 21:02:50 +0200

    refactor hello -> home

--- src/main/java/fr/mycook/controller/HomeController.java
+++ src/main/java/fr/mycook/controller/HomeController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController{
+public class HomeController{
 
 	@GetMapping(value = "/")
 	public String index() {
-		return "hello";
+		return "home";
 	}
 
 	@GetMapping(value = "/error")
