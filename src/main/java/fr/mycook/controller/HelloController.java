c28f6b134283f92fbe3b50aca01733253fdfbf90
owen <owenboutet@gmail.com>
2023-08-25 21:00:44 +0200

    remove class Humain

--- src/main/java/fr/mycook/controller/HelloController.java
+++ src/main/java/fr/mycook/controller/HelloController.java
@@ -1,19 +1,13 @@
 package fr.mycook.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import fr.mycook.repository.HumanRepository;
 
 @Controller
 public class HelloController{
 
-	@Autowired
-	HumanRepository humanRepository;
-
 	@GetMapping(value = "/")
 	public String index() {
-		
 		return "hello";
 	}
 
