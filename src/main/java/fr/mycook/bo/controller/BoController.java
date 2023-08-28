1e8d835b077e49ad52bee5a6ecec7d247f8cce76
owen <owenboutet@gmail.com>
2023-08-28 17:58:32 +0200

    ajout d'un controller BO et de sous controller pour gérer les pages

---
+++ src/main/java/fr/mycook/bo/controller/BoController.java
@@ -0,0 +1,10 @@
+package fr.mycook.bo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="bo")
+public interface BoController {
+    
+}
--- src/main/java/fr/mycook/bo/controller/BoRecepeController.java
+++ src/main/java/fr/mycook/bo/controller/BoRecepeController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BoHomeController implements BoController{
-    @GetMapping(value = {"", "/", "/home"})
+public class BoRecepeController implements BoController{
+    @GetMapping(value = {"/recepe"})
     public String home() {
-        return "bo/home";
+        return "bo/recepe";
     }
 }
